83fb13fca6273d9549b37bb0e148f04289c15d00
Yinyin-Huang <78446668+Yinyin-Huang@users.noreply.github.com>
2023-07-10 16:52:02 -0400

    start my_select

--- enumerables_array.rb
+++ enumerables_array.rb
@@ -2,11 +2,32 @@
 class Array
 
  def my_each(&block)
-  self.each do |num|
-    block.call(num)
-  end
+  self.map{|num| block.call(num)}
   return self
+ end
+
+ def my_select(&block)
+
+  if self.my_each(&block)
+  new_arr
  end
 
 end
 
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
+
+# return_value = [1, 2, 3].my_each do |num|
+#   puts num
+#  end.my_each do |num|
+#   puts num
+#  end
+#  # => 1
+#  #    2
+#  #    3
+#  #    1
+#  #    2
+#  #    3
+
+#  p return_value  # => [1, 2, 3]
